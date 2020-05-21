473b6910a111f8050c72587cf534549467ed482a
Anton Segerkvist <anton.stefan.roine.segerkvist@ericsson.com>
2020-05-21 12:38:23 +0200

    Addex exponential regression

--- regression.go
+++ regression.go
@@ -1,12 +1,23 @@
 package regression
 
-/**
- * ERRORS
- */
+import (
+	"errors"
+	"math"
+)
 
-/**
- * ENTITIES
- */
+/**********
+ * ERRORS *
+ **********/
+
+//
+// ErrUndeterminedSystem is returned when there are no solutions to
+// the regression system.
+//
+var ErrUndeterminedSystem error = errors.New("The system is undetermined")
+
+/************
+ * ENTITIES *
+ ************/
 
 //
 // Point2D32 contains coordinates in 2D euclidian space with 32 bit precision.
@@ -24,13 +35,13 @@ type Point2D64 struct {
 	Y float64
 }
 
-/**
- * REGRESSION
- */
+/**************
+ * REGRESSION *
+ **************/
 
-/**
- * LinearRegression2D32
- */
+/************************
+ * LinearRegression2D32 *
+ ************************/
 
 //
 // LinearRegression2D32 contains methods for performing linear regression on a
@@ -56,6 +67,10 @@ func NewLinearRegression2D32() *LinearRegression2D32 {
 //
 func (lg *LinearRegression2D32) Train(points *[]Point2D32) error {
 
+	if len(*points) < 2 {
+		return ErrUndeterminedSystem
+	}
+
 	n := float32(len(*points))
 
 	var ySum float32 = 0
@@ -80,15 +95,15 @@ func (lg *LinearRegression2D32) Train(points *[]Point2D32) error {
 }
 
 //
-// Predict calculates regression value at the given coordinate.
+// Predict calculates the regression value at the given coordinate.
 //
 func (lg LinearRegression2D32) Predict(x float32) float32 {
 	return lg.Coefficients[0] + lg.Coefficients[1]*x
 }
 
-/**
- * LinearRegression2D64
- */
+/************************
+ * LinearRegression2D64 *
+ ************************/
 
 //
 // LinearRegression2D64 contains methods for performing linear regression on a
@@ -112,7 +127,11 @@ func NewLinearRegression2D64() *LinearRegression2D64 {
 //
 // Train calculates the coefficient vector from the provided data set.
 //
-func (lg *LinearRegression2D64) Train(points *[]Point2D64) error {
+func (lr *LinearRegression2D64) Train(points *[]Point2D64) error {
+
+	if len(*points) < 2 {
+		return ErrUndeterminedSystem
+	}
 
 	n := float64(len(*points))
 
@@ -130,16 +149,136 @@ func (lg *LinearRegression2D64) Train(points *[]Point2D64) error {
 
 	var denominator = n*x2Sum - xSum*xSum
 
-	lg.Coefficients[0] = (ySum*x2Sum - xSum*xySum) / denominator
-	lg.Coefficients[1] = (n*xySum - xSum*ySum) / denominator
+	lr.Coefficients[0] = (ySum*x2Sum - xSum*xySum) / denominator
+	lr.Coefficients[1] = (n*xySum - xSum*ySum) / denominator
 
 	return nil
 
 }
 
 //
-// Predict calculates regression value at the given coordinate.
+// Predict calculates the regression value at the given coordinate.
 //
-func (lg LinearRegression2D64) Predict(x float64) float64 {
-	return lg.Coefficients[0] + lg.Coefficients[1]*x
+func (lr LinearRegression2D64) Predict(x float64) float64 {
+	return lr.Coefficients[0] + lr.Coefficients[1]*x
+}
+
+/*****************************
+ * ExponentialRegression2D32 *
+ *****************************/
+
+//
+// ExponentialRegression2D32 contains methods for performing exponential regression on a
+// two dimensional data set using 32 bit precission.
+//
+type ExponentialRegression2D32 struct {
+	Coefficients []float32
+}
+
+//
+// NewExponentialRegression2D32 creates an instance LinearRegression2D64.
+//
+func NewExponentialRegression2D32() *ExponentialRegression2D32 {
+	return &ExponentialRegression2D32{
+		Coefficients: []float32{0, 0},
+	}
+}
+
+//
+// Train calculates the coefficient vector from the provided data set.
+//
+func (er *ExponentialRegression2D32) Train(points *[]Point2D32) error {
+
+	if len(*points) < 2 {
+		return ErrUndeterminedSystem
+	}
+
+	n := float32(len(*points))
+
+	var ySum float32 = 0
+	var xSum float32 = 0
+	var x2Sum float32 = 0
+	var xySum float32 = 0
+
+	for _, v := range *points {
+		ySum += float32(math.Log(float64(v.Y)))
+		xSum += v.X
+		x2Sum += v.X * v.X
+		xySum += v.X * float32(math.Log(float64(v.Y)))
+	}
+
+	var denominator = n*x2Sum - xSum*xSum
+
+	er.Coefficients[0] = float32(math.Exp(float64((ySum*x2Sum - xSum*xySum) / denominator)))
+	er.Coefficients[1] = (n*xySum - xSum*ySum) / denominator
+
+	return nil
+
+}
+
+//
+// Predict calculates the regression value at the given coordinate.
+//
+func (er ExponentialRegression2D32) Predict(x float32) float32 {
+	return er.Coefficients[0] * float32(math.Exp(float64(er.Coefficients[1]*x)))
+}
+
+/*****************************
+ * ExponentialRegression2D64 *
+ *****************************/
+
+//
+// ExponentialRegression2D64 contains methods for performing exponential regression on a
+// two dimensional data set using 64 bit precission.
+//
+type ExponentialRegression2D64 struct {
+	Coefficients []float64
+}
+
+//
+// NewExponentialRegression2D64 creates an instance LinearRegression2D64.
+//
+func NewExponentialRegression2D64() *ExponentialRegression2D64 {
+	return &ExponentialRegression2D64{
+		Coefficients: []float64{0, 0},
+	}
+}
+
+//
+// Train calculates the coefficient vector from the provided data set.
+//
+func (er *ExponentialRegression2D64) Train(points *[]Point2D64) error {
+
+	if len(*points) < 2 {
+		return ErrUndeterminedSystem
+	}
+
+	n := float64(len(*points))
+
+	var ySum float64 = 0
+	var xSum float64 = 0
+	var x2Sum float64 = 0
+	var xySum float64 = 0
+
+	for _, v := range *points {
+		ySum += math.Log(v.Y)
+		xSum += v.X
+		x2Sum += v.X * v.X
+		xySum += v.X * math.Log(v.Y)
+	}
+
+	var denominator = n*x2Sum - xSum*xSum
+
+	er.Coefficients[0] = math.Exp((ySum*x2Sum - xSum*xySum) / denominator)
+	er.Coefficients[1] = (n*xySum - xSum*ySum) / denominator
+
+	return nil
+
+}
+
+//
+// Predict calculates the regression value at the given coordinate.
+//
+func (er ExponentialRegression2D64) Predict(x float64) float64 {
+	return er.Coefficients[0] * math.Exp(er.Coefficients[1]*x)
 }
--- regression_test.go
+++ regression_test.go
@@ -94,3 +94,73 @@ func TestLinearRegression2D64(t *testing.T) {
 	}
 
 }
+
+func TestExponentialRegression2D32(t *testing.T) {
+
+	tests := [][]regression.Point2D32{
+		[]regression.Point2D32{
+			{X: 1, Y: 1},
+			{X: 2, Y: 2},
+		},
+		[]regression.Point2D32{
+			{X: 1, Y: 3},
+			{X: 3, Y: 1},
+			{X: 4, Y: 2},
+		},
+	}
+
+	predictions := []float32{
+		0.5,
+		3.050905515,
+	}
+
+	exponentialRegression := regression.NewExponentialRegression2D32()
+
+	for i, test := range tests {
+		err := exponentialRegression.Train(&test)
+		if err != nil {
+			t.Errorf("Got prediction error; expected prediction %f", predictions[i])
+		}
+		prediction := exponentialRegression.Predict(0)
+
+		if math.Abs(float64(prediction-predictions[i])) >= PRECISSION_EPS {
+			t.Errorf("Got prediction %f; expected prediction %f", prediction, predictions[i])
+		}
+	}
+
+}
+
+func TestExponentialRegression2D64(t *testing.T) {
+
+	tests := [][]regression.Point2D64{
+		[]regression.Point2D64{
+			{X: 1, Y: 1},
+			{X: 2, Y: 2},
+		},
+		[]regression.Point2D64{
+			{X: 1, Y: 3},
+			{X: 3, Y: 1},
+			{X: 4, Y: 2},
+		},
+	}
+
+	predictions := []float64{
+		0.5,
+		3.050905515,
+	}
+
+	exponentialRegression := regression.NewExponentialRegression2D64()
+
+	for i, test := range tests {
+		err := exponentialRegression.Train(&test)
+		if err != nil {
+			t.Errorf("Got prediction error; expected prediction %f", predictions[i])
+		}
+		prediction := exponentialRegression.Predict(0)
+
+		if math.Abs(prediction-predictions[i]) >= PRECISSION_EPS {
+			t.Errorf("Got prediction %f; expected prediction %f", prediction, predictions[i])
+		}
+	}
+
+}
